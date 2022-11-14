08a786786a430a45c255bd87d9772b2840a20fbb
Norbus2000 <odornorbert.on@gmail.com>
2022-11-14 11:06:09 +0100

    Elso feladat

--- src/dolgozat_oop/Dolgozat_oop.java
+++ src/dolgozat_oop/Dolgozat_oop.java
@@ -1,20 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package dolgozat_oop;
 
-/**
- *
- * @author odor.norbert
- */
 public class Dolgozat_oop {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new gui().setVisible(true);
+            }
+        });
     }
-    
+
 }
